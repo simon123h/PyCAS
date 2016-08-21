4d7ddb785d92b61c915e0c6a734f8cb87957cdb7
simon123h <simonastronomie@gmail.com>
2016-08-22 01:30:35 +0200

    ''

--- GitRemote.bat
+++ GitRemote.bat
@@ -8,18 +8,26 @@ goto choice
 
 
 :more
+echo [R]ecommit
 echo [U]pdate from server
+echo [S]tatus
+echo [D]ifference in files
+echo [I]nit new repo
 echo [E]xit
 goto choice
 
 
 :choice
-choice /c npmue /n /m ">>> "
+choice /c npmueisdr /n /m ">>> "
 if %errorlevel%==1 goto :newCommit
 if %errorlevel%==2 goto :push
 if %errorlevel%==3 goto :more
 if %errorlevel%==4 goto :pull
 if %errorlevel%==5 goto :exit
+if %errorlevel%==6 goto :init
+if %errorlevel%==7 goto :status
+if %errorlevel%==8 goto :diff
+if %errorlevel%==8 goto :recommit
 
 
 :startOver
@@ -36,6 +44,13 @@ set /p msg="Commit message: "
 git commit -m '%msg%'
 goto startOver
 
+:newCommit
+echo.
+git add .
+set /p msg="Commit message: "
+git commit -m '%msg%' --amend
+goto startOver
+
 
 :push
 echo.
@@ -49,7 +64,25 @@ git pull
 goto startOver
 
 
-:fastCommitPush
+:init
+echo.
+git init
+set /p url="Clone URL: "
+if not "%url%" == "" git clone %url%
+goto startOver
+
+
+:status
+echo.
+git status
+goto startOver
+
+
+:diff
+echo.
+git diff
+goto startOver
+
 
 
 :exit
