c4af994d792de77d1c250afce29015b6365742ae
simon123h <simonastronomie@gmail.com>
2017-04-15 02:34:07 +0200

    replace TRSCAS references with PyCas

--- __init__.py
+++ __init__.py
@@ -6,8 +6,8 @@
 # IMPORTANT: for the whole CAS to work as a package,
 # the subpackages will have to prepend the CAS package name in front of
 # their imports! e.g. in trs subpackage file:
-# from TRSCAS.mathexpr.expr import Expression
+# from PyCas.mathexpr.expr import Expression
 
 
-from TRSCAS.mathexpr import * # noqa
-import TRSCAS.rules as rules # noqa
+from PyCas.mathexpr import * # noqa
+import PyCas.rules as rules # noqa
